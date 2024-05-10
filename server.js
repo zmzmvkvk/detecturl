bc7c2811d743df0c168704f87dde8cc40ae83922
김서준 <gimseojun@gimseojun-ui-MacBookAir.local>
2024-05-10 09:33:12 +0900

    test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const bot = new TelegramBot(token, {
         }
     }
 });
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 async function connectDatabase() {
     const client = await MongoClient.connect(dburl);
