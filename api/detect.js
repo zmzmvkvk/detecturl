7e4ea9afefdd210510ba42c616b5dbf26c89edb9
김서준 <gimseojun@gimseojun-ui-MacBookAir.local>
2024-05-17 14:08:09 +0900

    test

--- api/detect.js
+++ api/detect.js
@@ -1,30 +1,23 @@
 require("dotenv").config();
-const express = require("express");
 const { MongoClient } = require("mongodb");
 const { Builder, By, until } = require("selenium-webdriver");
 const chrome = require('selenium-webdriver/chrome');
-const app = express();
+const schedule = require('node-schedule');
+const TelegramBot = require('node-telegram-bot-api');
+
+// 환경 변수 설정
 const dbUser = process.env.DB_USER;
 const dbPassword = encodeURIComponent(process.env.DB_PASSWORD);
 const dbName = "richlab";
-const PORT = process.env.PORT || 3000;
 const dburl = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.xseitpb.mongodb.net/`;
-const schedule = require('node-schedule');
 const chatId = process.env.TELE_CHAT_ID;
 const chatId2 = process.env.TELE_CHAT_ID_SH;
 const token = process.env.TELE_BOT_TOKEN;
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(token, {
-    polling: true,
-    request: {
-        agentOptions: {
-            keepAlive: true,
-            family: 4
-        }
-    }
-});
+
+const bot = new TelegramBot(token, { polling: true, request: { agentOptions: { keepAlive: true, family: 4 } } });
 let db, driver;
 
+// 데이터베이스 연결
 async function connectDatabase() {
     const client = await MongoClient.connect(dburl);
     console.log("DB연결성공");
@@ -33,6 +26,7 @@ async function connectDatabase() {
     scheduleJob();
 }
 
+// 드라이버 초기화
 async function initDriver() {
     driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options()
         .addArguments(
@@ -45,6 +39,7 @@ async function initDriver() {
         )).build();
 }
 
+// 스케줄러 설정
 async function scheduleJob() {
     schedule.scheduleJob('*/1 * * * *', async function () {
         const detections = await fetchDetections();
@@ -54,50 +49,46 @@ async function scheduleJob() {
     });
 }
 
+// 탐지 설정 가져오기
 async function fetchDetections() {
     return db.collection("detect").find({}).toArray().then(results => {
         return results.map(doc => ({ url: doc.url, buttonType: doc.buttonType, text: doc.text }));
     });
 }
 
+// URL에서 요소 생성
 async function generate({ url, buttonType, text }) {
+    const text = []
     try {
         await driver.get(url);
         const button = await driver.wait(until.elementLocated(By.css(buttonType)), 2000);
         const buttonText = await button.getText();
         if (buttonText === text) {
             console.log(`STATUS : SOLD OUT for ${url}`);
+
         } else {
-            console.log(`STATUS : AVAILABLE for ${url}`);
             console.log(`buttonText = ${buttonText}`);
             console.log(`Text = ${text}`);
+            console.log(`STATUS : AVAILABLE for ${url}`);
             // await bot.sendMessage(chatId, `재고 입고된듯 ${url}`);
             // await bot.sendMessage(chatId2, `재고 입고된듯 ${url}`);
         }
     } catch (error) {
         console.log(`Error processing ${url}: ${error}`);
     }
+    // 봇 연결
+    // bot.onText(/\/add (.+)/, async (msg, match) => {
+    //     const msgchatId = msg.chat.id;
+    //     const resp = match[1];
+    //     console.log(resp);
+    //     console.log(`chatId = ${msgchatId}`);
+    //     try {
+    //         await db.collection("detect").insertOne({ url: resp });
+    //         bot.sendMessage(msgchatId, `${resp} 가 db에 저장되었습니다.`);
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // });
 }
 
-connectDatabase().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-    
-    bot.onText(/\/add (.+)/, async (msg, match) => {
-        const msgchatId = msg.chat.id;
-        const resp = match[1];
-    
-        console.log(resp);
-        console.log(`chatId = ${msgchatId}`);
-        try {
-            await db.collection("detect").insertOne({ url: resp });
-        } catch (e) {
-            console.log(e);
-        }
-
-        bot.sendMessage(msgchatId, `${resp} 가 db에 저장되었습니다.`);
-    });
-}).catch(err => {
-    console.error("Database connection failed", err);
-});
+module.exports = { connectDatabase };
